2b515d3b0ab162f5341f0918da17046d9175c282
Kuznetsov Konstantin <kuznetsov@marmalade.de>
2014-01-15 16:30:55 +0100

    add metadata and marmalade logo

---
+++ metadata.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.1';
+ 
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'           => 'csvexporter',
+    'title'        => 'CSV Exporter',
+    'description'  => 'Export your products to csv file.',
+    'thumbnail'    => 'marmalade.jpg',
+    'version'      => '1.0',
+    'author'       => 'marmalade GmbH',
+    'url'          => 'http://www.marmalade.de',
+    'email'        => 'support@marmalade.de',
+    'extend'       => array(),
+);
